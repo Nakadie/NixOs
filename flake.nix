5c631ca4b28fef9840c02bf02e3c9c469948e844
Nakadie <kamueladietiker@gmail.com>
2026-08-22 13:11:19 +0900

    add open code

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
-    vscode-server.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, vscode-server, ... }@inputs: {
@@ -13,6 +13,7 @@
       modules = [
         vscode-server.nixosModules.default
         ./configuration.nix
+        ./opencode.nix
         # Enable the service here as an inline module:
         ({ config, pkgs, ... }: {
           services.vscode-server.enable = true;
